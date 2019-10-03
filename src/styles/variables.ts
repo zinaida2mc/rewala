916690d7b375cf676341cc4e369c4c451e2a8f1f
alex <Zinaida.d@2muchcoffee.com>
2019-10-03 12:32:41 +0300

    feat(src:styles) implemented variables set up for colors & font-family & font-size

---
+++ src/styles/variables.ts
@@ -0,0 +1,24 @@
+export const ColorVariables = {
+  black: '#000',
+  white: '#fff',
+  grey: '#e0e0e0',
+  lightTeal: '#80cbc4',
+  darkTeal: '#00796b',
+  error: '#ef5350',
+  warning: '#ffe082',
+  success: '#81c784'
+};
+
+export const FontSizeVariables = {
+  small: 14,
+  semiSmall: 16,
+  middle: 20,
+  h1: 28,
+  h2: 24,
+};
+
+export const FontFamilyVariables = {
+  regular: 'Quicksand-Regular',
+  medium: 'Quicksand-Medium',
+  bold: 'Quicksand-Bold',
+};
