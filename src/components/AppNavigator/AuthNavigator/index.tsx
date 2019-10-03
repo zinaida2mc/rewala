12c5ebbb5dfe3f868256d96c4bb1da1a55811e83
alex <Zinaida.d@2muchcoffee.com>
2019-10-03 17:56:41 +0300

    feat(src:components:AppNavigator:AuthNavigator) connected Login screen navigation to app

--- src/components/AppNavigator/AuthNavigator/index.tsx
+++ src/components/AppNavigator/AuthNavigator/index.tsx
@@ -1,12 +1,14 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import RegistrationScreen from './RegistrationScreen';
+import LoginScreen from './LoginScreen/index';
 
 export const AuthNavigator = createStackNavigator(
   {
     RegistrationScreen,
+    LoginScreen
   },
   {
     headerMode: 'none',
-    initialRouteName: 'RegistrationScreen',
+    initialRouteName: 'LoginScreen',
   },
 );
