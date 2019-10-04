45f821dd42f1628372eda0733a614173757d351d
alex <Zinaida.d@2muchcoffee.com>
2019-10-04 19:11:39 +0300

    feat(src:components:AppNavigator:AuthNavigator:index) connected reset password code screen to app

--- src/components/AppNavigator/AuthNavigator/index.tsx
+++ src/components/AppNavigator/AuthNavigator/index.tsx
@@ -2,15 +2,17 @@ import { createStackNavigator } from 'react-navigation-stack';
 import RegistrationScreen from './RegistrationScreen';
 import LoginScreen from './LoginScreen/index';
 import ResetPasswordScreen from './ResetPasswordScreen/index';
+import EnterCodeScreen from './EnterCodeScreen/index';
 
 export const AuthNavigator = createStackNavigator(
   {
     RegistrationScreen,
     LoginScreen,
     ResetPasswordScreen,
+    EnterCodeScreen,
   },
   {
     headerMode: 'none',
-    initialRouteName: 'ResetPasswordScreen',
+    initialRouteName: 'LoginScreen',
   },
 );
