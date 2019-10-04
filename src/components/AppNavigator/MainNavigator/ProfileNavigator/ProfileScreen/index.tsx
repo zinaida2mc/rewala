772bbb7dcf878c00e9da58f856b1ca0d72693ede
alex <Zinaida.d@2muchcoffee.com>
2019-10-04 13:01:57 +0300

    feat(src:components:AppNavigator:ProfileNavigator:ProfileScreen) implemented getting & displaying user info

--- src/components/AppNavigator/MainNavigator/ProfileNavigator/ProfileScreen/index.tsx
+++ src/components/AppNavigator/MainNavigator/ProfileNavigator/ProfileScreen/index.tsx
@@ -1,12 +1,58 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text, View } from 'react-native';
+import { style } from './style';
+import { CommonButton } from '../../../../../shared/components/common-button/index';
+import { Dispatch } from 'redux';
+import { Actions } from '../../../../../store/auth/actions';
+import { connect } from 'react-redux';
+import { RootState } from '../../../../../store/index';
 
-const ProfileScreen = () => {
+const mapStateToProps = (state: RootState) => ({
+  userData: state.auth.userData,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  getMe: () => dispatch(Actions.getMe()),
+});
+
+type Props = ReturnType<typeof mapDispatchToProps> & ReturnType<typeof mapStateToProps>;
+
+const ProfileScreen: React.FC<Props> = ({ getMe, userData }) => {
+  useEffect(() => {
+    getMe();
+  }, [getMe]);
+
+  if (!userData) {
+    return <Text>L O A D I N G</Text>;
+  }
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Profile Screen</Text>
+    <View style={style.root}>
+      <View>
+        <Text style={style.header}>Welcome to your profile!</Text>
+      </View>
+
+      <View>
+        <Text style={style.userInfo}>Name: {userData.profile.fullName}</Text>
+        <Text style={style.userInfo}>Email: {userData.email}</Text>
+        <Text style={style.userInfo}>
+          Phone: {userData.profile.countryCode} {userData.profile.phone}
+        </Text>
+      </View>
+
+      <View>
+        <CommonButton
+          title={'Log out'}
+          type={'outline'}
+          buttonStyle={style.button}
+          titleStyle={style.buttonTitle}
+        />
+      </View>
     </View>
   );
 };
 
-export default ProfileScreen;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ProfileScreen);
---
+++ src/components/AppNavigator/MainNavigator/ProfileNavigator/ProfileScreen/style.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+import { ColorVariables, FontFamilyVariables, FontSizeVariables } from '../../../../../styles/variables';
+
+export const style = StyleSheet.create<StyleSheet.NamedStyles<any>>({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  header: {
+    fontFamily: FontFamilyVariables.medium,
+    fontSize: FontSizeVariables.h2,
+    color: ColorVariables.darkTeal,
+  },
+  userInfo: {
+    fontFamily: FontFamilyVariables.medium,
+    fontSize: FontSizeVariables.middle,
+  },
+  button: {
+    backgroundColor: ColorVariables.lightTeal,
+    minWidth: 200,
+    maxWidth: 250,
+  },
+  buttonTitle: {
+    color: ColorVariables.darkTeal,
+  }
+});
