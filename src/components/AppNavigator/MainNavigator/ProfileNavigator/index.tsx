69744fa6ad920bb8131fcc0b427786f007e4d7b5
alex <Zinaida.d@2muchcoffee.com>
2019-10-07 13:53:19 +0300

    feat(src:components:AppNavigator:MainNavigator:ProfileNavigator) connected change password screen to app

--- src/components/AppNavigator/MainNavigator/ProfileNavigator/index.tsx
+++ src/components/AppNavigator/MainNavigator/ProfileNavigator/index.tsx
@@ -1,11 +1,18 @@
 import { createStackNavigator } from 'react-navigation-stack';
 
 import ProfileScreen from './ProfileScreen';
+import ChangePasswordScreen from './ChangePasswordScreen';
 
 export const ProfileNavigator = createStackNavigator(
   {
     ProfileScreen: {
       screen: ProfileScreen,
+      navigationOptions: {
+        header: null,
+      },
+    },
+    ChangePasswordScreen: {
+      screen: ChangePasswordScreen,
       navigationOptions: {
         header: null,
       },
